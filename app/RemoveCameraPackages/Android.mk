e141af813db6326e8eb2787582c7d8e72303da65
Sarthak Roy <sarthakroy2002@gmail.com>
2023-06-18 18:24:17 +0530

    RMX2020: RemoveCameraPackages: Nuke GoogleCameraGo
    Change-Id: Ifd18615bf2402e0db766cd289ba9a05fa9ae0510
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- app/RemoveCameraPackages/Android.mk
+++ app/RemoveCameraPackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemoveCameraPackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Snap Camera2 GCamGOPrebuilt-V3 GCamGOPrebuilt-V2 GCamGOPrebuilt GrapheneCamera
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2 GCamGOPrebuilt-V3 GCamGOPrebuilt-V2 GCamGOPrebuilt GrapheneCamera GoogleCameraGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
